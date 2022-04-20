ec4bd4b1ae4833fd42cf4c467bd4951f5c9cd6f2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 15:58:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
 st.image(yt_img)
 st.write('YouTube video thumbnail image URL: ', yt_img)
 
-st.write(yt)
+st.write(st.experimental_get_query_params)
