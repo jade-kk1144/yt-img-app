8bd4840f964954ebbfeedf41ec841d4d2e8e4bf3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 13:51:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write(st.experimental_get_query_params())
 yt = st.experimental_get_query_params()['yt'][0]
 
 if 'youtu.be' in yt:
-  a = yt.split('/')
+  a = yt.split('/')[-1]
   st.write(a)
 
 #f'http://img.youtube.com/vi/{yt}/0.jpg'
