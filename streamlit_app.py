560db5b38058accc3ff4c4cbbbf34499ffd7d765
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 15:49:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.title('st.experimental_get_query_params')
 
-st.header('Query text from URL')
-st.write(st.experimental_get_query_params())
-
 # Image settings
 st.sidebar.header('Settings')
 img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
@@ -15,19 +12,15 @@ st.header('Applying query text from URL')
 # Retrieving YouTube video ID from URL
 yt = st.experimental_get_query_params()['yt'][0]
 
+if 'youtu.be' in yt:
+  ytid = yt.split('/')[-1]
+  st.write('YouTube video ID: ', ytid)
+if 'youtube.com' in yt:
+  ytid = yt.split('=')[-1]
+  st.write('YouTube video ID: ', ytid)
 
-if yt is not None:
-  if 'youtu.be' in yt:
-    ytid = yt.split('/')[-1]
-    st.write('YouTube video ID: ', ytid)
-  if 'youtube.com' in yt:
-    ytid = yt.split('=')[-1]
-    st.write('YouTube video ID: ', ytid)
+# Display YouTube thumbnail image
+yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
 
-  # Display YouTube thumbnail image
-  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
-
-  st.image(yt_img)
-  st.write('YouTube video thumbnail image URL: ', yt_img)
-else:
-  st.write('Awaiting')
+st.image(yt_img)
+st.write('YouTube video thumbnail image URL: ', yt_img)
