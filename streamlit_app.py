33c9f14366b4956f3f4f465d7b4e20a8608e9d48
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 16:46:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,18 @@ import streamlit as st
 
 st.title('st.experimental_get_query_params')
 
+# Update parameter
+def update_params():
+    st.experimental_set_query_params(yt=st.session_state.yt)
+
 # Image settings
 st.sidebar.header('Settings')
 img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
 selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
 img_quality = img_dict[selected_img_quality]
 
-yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/DctmeFx8s_k')
+yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/DctmeFx8s_k', key='day', on_change=update_params)
+
 
 # Retrieving YouTube video ID from URL
 yt = st.experimental_get_query_params()['yt'][0]
