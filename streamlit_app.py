d70ac695d0966e5ba9663cdb716f9e9f45144e6e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 17:06:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,13 @@ import streamlit as st
 
 st.title('st.experimental_get_query_params')
 
-# Update parameter
-def update_params():
-    st.experimental_set_query_params(yt=st.session_state.yt)
-
-#query_params = st.experimental_get_query_params()
-
-if 'yt' not in st.session_state:
-    st.session_state.yt = ''
-st.info(st.session_state.yt)
-    
 # Image settings
 st.sidebar.header('Settings')
 img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
 selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
 img_quality = img_dict[selected_img_quality]
 
-yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/DctmeFx8s_k', key='yt', on_change=update_params)
-
+yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/DctmeFx8s_k')
 
 # Retrieving YouTube video ID from URL
 #yt = st.experimental_get_query_params()['yt'][0]
@@ -39,6 +28,3 @@ ytid = get_ytid(yt_url) # yt or yt_url
 yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
 st.image(yt_img)
 st.write('YouTube video thumbnail image URL: ', yt_img)
-
-#st.write('yt: ', yt)
-#st.write('yt_url: ', yt_url)
