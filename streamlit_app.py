501ac732a006f44fa9fba3929ea1112ed1ac4ca2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 13:52:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ st.write(st.experimental_get_query_params())
 yt = st.experimental_get_query_params()['yt'][0]
 
 if 'youtu.be' in yt:
-  a = yt.split('/')[-1]
-  st.write(a)
-
+  ytid = yt.split('/')[-1]
+  st.write(ytid)
+if 'youtube.com' in yt:
+  ytid = yt.split('=')[-1]
+  st.write(ytid)
+  
 #f'http://img.youtube.com/vi/{yt}/0.jpg'
