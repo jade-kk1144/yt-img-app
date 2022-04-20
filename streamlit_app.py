f22074498f624c9beba35f517dbb352da6c60e50
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 13:32:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write(st.experimental_get_query_params())
 
-yt = st.experimental_get_query_params()['yt'][0].split()
+yt = st.experimental_get_query_params()['yt'][0].split('=')
 
 #f'http://img.youtube.com/vi/{yt}/0.jpg'
