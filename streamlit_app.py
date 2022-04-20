a04b37ed3ed5e5e10feecdd74373a5a0ec3ca118
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 14:11:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.write(st.experimental_get_query_params())
 
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard']
+img_quality = img_dict[selected_img_quality]
+
+
 yt = st.experimental_get_query_params()['yt'][0]
 
 if 'youtu.be' in yt:
@@ -10,8 +16,8 @@ if 'youtu.be' in yt:
 if 'youtube.com' in yt:
   ytid = yt.split('=')[-1]
   st.write(ytid)
-  
-yt_img = f'http://img.youtube.com/vi/{ytid}/maxresdefault.jpg'
+
+yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
 
 st.image(yt_img)
 st.write(yt_img)
