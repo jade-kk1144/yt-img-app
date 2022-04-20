19b99d41810c8edbe95e1572d254678dc5c59792
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 12:05:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,15 @@
 import streamlit as st
 
 def update_params():
-    st.experimental_set_query_params(challenge=st.session_state.day)
-
-days_list = [f'Day {x}' for x in [1,2,3,4,5]]
+    st.experimental_set_query_params(q=st.session_state.q)
 
 query_params = st.experimental_get_query_params()
 
-st.write(query_params)
-
-if query_params and query_params["challenge"][0] in days_list:
-    st.session_state.day = query_params["challenge"][0]
+st.write(query_params["q"][0])
 
 selected_day = st.selectbox('Start the Challenge 👇', days_list, key="day", on_change=update_params)
 for i in days_list:
     if selected_day == i:
         st.markdown(f'# 🗓️ {i}')
 
-st.write(query_params)
-st.write('st.session_state.day: ', st.session_state.day)
+st.write('st.session_state.q: ', st.session_state.q)
